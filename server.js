11ffa115807aab8cf12c3fb3cb4b3c31233bdf29
haol <171213574@qq.com>
2015-08-29 16:11:59 +0800

    结构优化

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 var express = require('express'),
 	bodyParser = require('body-parser'),
-    // wine = require('./routes/wines');
     moogoose = require('./models/moogoose'),
     user = require('./routes/user');
 
 
+//setting
 var app = express();
 app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/public', express.static(__dirname + '/public'));
 
+
+//routes
 app.post('/user/add',user.douserAdd);
 app.get('/user/findByName/:name',user.findByName);
 app.get('/user/findUserById/:id',user.findUserById);
 app.get('/user/find/all',user.findAll);
 app.delete('/user/deleteByName/:name',user.deleteByName);
 
-// app.get('/wines', wine.findAll);
-// app.get('/wines/:id', wine.findById);
-// app.post('/wines', wine.addWine);
-// app.put('/wines/:id', wine.updateWine);
-// app.delete('/wines/:id', wine.deleteWine);
 
+//server port
 app.listen(8080);
 console.log('Listening on port 8080...');
